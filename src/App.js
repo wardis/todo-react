ea624c8fd9c5fc394ab9f0314f2644cdcfd6dc69
wardis <wardi2976@live.fr>
2019-11-10 23:34:33 +0000

    functional to class App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      Hello Didi!
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        Hello Didi!
+      </div>
+    );
+  }
 }
 
 export default App;
