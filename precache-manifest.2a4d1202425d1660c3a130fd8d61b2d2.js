5f9487939fa264c2c3f331729e60190666b849fd
wardis <wardi2976@live.fr>
2019-11-11 22:25:55 +0000

    Updates

---
+++ precache-manifest.2a4d1202425d1660c3a130fd8d61b2d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "382a87a0188d04d764720d1af3cfe33e",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "a7a8ee1cf1a797c40e2d",
+    "url": "/todo/static/css/main.2ec23026.chunk.css"
+  },
+  {
+    "revision": "3de678a348ff109e4d58",
+    "url": "/todo/static/js/2.085e6757.chunk.js"
+  },
+  {
+    "revision": "a7a8ee1cf1a797c40e2d",
+    "url": "/todo/static/js/main.d1e21d86.chunk.js"
+  },
+  {
+    "revision": "fa403113ead66244d282",
+    "url": "/todo/static/js/runtime-main.0fecbaf7.js"
+  }
+]);
